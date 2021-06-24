17bc11863f121316d36a70a4d6d66af4e6677e65
snarlynarwhal <pjlegendreiv@gmail.com>
2021-06-23 22:11:09 -0400

    Initial commit

---
+++ Runtime/Scripts/CustomYieldInstructions/WaitForAnimatorState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace HHG.Common
+{
+    public class WaitForAnimatorState : CustomYieldInstruction
+    {
+        public override bool keepWaiting {
+            get {
+                AnimatorStateInfo stateInfo = Animator.GetCurrentAnimatorStateInfo(0);
+                if (stateInfo.IsName(State) || stateInfo.IsTag(State))
+                {
+                    return stateInfo.normalizedTime < NormalizedTime;
+                }
+                return true;
+            }
+        }
+
+        public Animator Animator { get; set; }
+        public string State { get; set; }
+        public float NormalizedTime { get; set; }
+
+        public WaitForAnimatorState(Animator animator, string state, float normalizedTime = 0)
+        {
+            Animator = animator;
+            State = state;
+            NormalizedTime = normalizedTime;
+        }
+    }
+}
---
+++ Runtime/Scripts/Structs/SubjectId.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HHG.Common
+{
+    public struct SubjectId : IEquatable<SubjectId>
+    {
+        public Type Type { get; set; }
+        public object Id { get; set; }
+
+        public SubjectId(Type type, object id)
+        {
+            Type = type;
+            Id = id;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                hash = hash * 29 + Type.GetHashCode();
+                hash = hash * 29 + (Id == null ? 0 : Id.GetHashCode());
+                return hash;
+            }
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other is SubjectId)
+            {
+                SubjectId otherId = (SubjectId) other;
+                return otherId == this;
+            }
+
+            return false;
+        }
+
+        public bool Equals(SubjectId other)
+        {
+            return this == other;
+        }
+
+        public static bool operator ==(SubjectId left, SubjectId right)
+        {
+            return left.Type == right.Type && Equals(left.Id, right.Id);
+        }
+
+        public static bool operator !=(SubjectId left, SubjectId right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
