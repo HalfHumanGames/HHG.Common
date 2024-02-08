76910313a4cfbb22665a480072b230b5cbfba7cb
snarlynarwhal <pjlegendreiv@gmail.com>
2024-02-07 19:37:35 -0500

    Added getting/setting proxy from binding

--- Runtime/Scripts/DataBinding/DataBinding.cs
+++ Runtime/Scripts/DataBinding/DataBinding.cs
@@ -7,13 +7,19 @@ namespace HHG.Common
         private readonly DataProxy<TMember> proxy;
         private readonly Action<TMember> setter;
 
+        public TMember Value
+        {
+            get => proxy;
+            set => proxy.Value = value;
+        }
+
         internal DataBinding(DataProxy<TMember> source, Action<TMember> set = null)
         {
             proxy = source;
             setter = set ?? (v => { });
         }
 
-        public void Set(TMember value) => setter(value);
+        internal void Set(TMember value) => setter(value);
 
         public void Release() => proxy.Unbind(this);
 
