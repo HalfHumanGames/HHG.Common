bb33e2cf09372fda60b3e55fdde42ccc6e0bf4d2
snarlynarwhal <pjlegendreiv@gmail.com>
2024-05-04 20:29:01 -0400

    Improved dropdown performance by caching dropdown options and type information

--- Editor/Scripts/Property Drawers/DropdownDrawer.cs
+++ Editor/Scripts/Property Drawers/DropdownDrawer.cs
@@ -1,18 +1,23 @@
 using HHG.Common.Runtime;
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Object = UnityEngine.Object;
 
 namespace HHG.Common.Editor
 {
     [CustomPropertyDrawer(typeof(DropdownAttribute))]
     public class DropdownDrawer : PropertyDrawer
     {
+        private static Dictionary<Type, List<Object>> assetCache = new Dictionary<Type, List<Object>>();
+        private static Dictionary<Type, List<string>> nameCache = new Dictionary<Type, List<string>>();
+
         private DropdownAttribute filter => attribute as DropdownAttribute;
-        private List<Object> choiceAssets = new List<Object>();
-        private List<string> choiceNames = new List<string>();
+        private List<Object> assets = new List<Object>();
+        private List<string> names = new List<string>();
 
         public override VisualElement CreatePropertyGUI(SerializedProperty property)
         {
@@ -26,7 +31,7 @@ namespace HHG.Common.Editor
             else
             {
                 RefreshDropdownValues(property);
-                DropdownField dropdownField = new DropdownField(property.displayName, choiceNames, 0);
+                DropdownField dropdownField = new DropdownField(property.displayName, names, 0);
                 dropdownField.AddToClassList(BaseField<string>.alignedFieldUssClassName);
                 dropdownField.AddManipulator(CreateDropdownContextMenu(dropdownField));
                 dropdownField.UnregisterValueChangedCallback(OnSpawnDropdownChanged);
@@ -39,27 +44,39 @@ namespace HHG.Common.Editor
             return container;
         }
 
-        private void RefreshDropdownValues(SerializedProperty property)
+        private void RefreshDropdownValues(SerializedProperty property, bool force = false)
         {
-            System.Type type = filter.Type ?? property.GetPropertyType();
-            DropdownUtil.GetChoiceList(ref choiceAssets, ref choiceNames, t => t.IsBaseImplementationOf(type), filter.filter);
+            Type type = filter.Type ?? property.GetPropertyType();
+            if (!force && assetCache.ContainsKey(type) && nameCache.ContainsKey(type))
+            {
+                assets = assetCache[type];
+                names = nameCache[type];     
+            }
+            else
+            {
+                DropdownUtil.GetChoiceList(ref assets, ref names, t => t.IsBaseImplementationOf(type), filter.filter);
+
+                // Cache for subsequent lookups
+                assetCache[type] = assets;
+                nameCache[type] = names;
+            }
         }
 
         private ContextualMenuManipulator CreateDropdownContextMenu(VisualElement visualElement) => new ContextualMenuManipulator((evt) =>
         {
-            evt.menu.AppendAction("Refresh", (x) => RefreshDropdownValues((SerializedProperty)visualElement.userData), DropdownMenuAction.AlwaysEnabled, visualElement);
+            evt.menu.AppendAction("Refresh", (x) => RefreshDropdownValues((SerializedProperty)visualElement.userData, true), DropdownMenuAction.AlwaysEnabled, visualElement);
         });
 
         private void OnSpawnDropdownChanged(ChangeEvent<string> evt)
         {
             DropdownField dropdownField = evt.currentTarget as DropdownField;
             SerializedProperty property = dropdownField.userData as SerializedProperty;
-            int i = choiceNames.IndexOf(evt.newValue);
+            int i = names.IndexOf(evt.newValue);
 
             // For some reason this event triggers for the label change as well
             if (i != -1)
             {
-                property.objectReferenceValue = choiceAssets[i];
+                property.objectReferenceValue = assets[i];
                 property.serializedObject.ApplyModifiedProperties();
                 dropdownField.SetValueWithoutNotify(property.objectReferenceValue?.name ?? "None");
             }
--- Runtime/Scripts/Extensions/TypeExtensions.cs
+++ Runtime/Scripts/Extensions/TypeExtensions.cs
@@ -6,13 +6,29 @@ namespace HHG.Common.Runtime
 {
     public static class TypeExtensions
     {
+        private static IEnumerable<Type> types;
+        private static Dictionary<Type, IEnumerable<Type>> subclasses;
+
         public static List<Type> FindSubclasses(this Type type, Func<Type, bool> filter = null)
         {
             filter ??= _ => true;
-            return AppDomain.CurrentDomain.GetAssemblies().
-                SelectMany(assembly => assembly.GetTypes()).
-                Where(t => t.IsClass && type.IsAssignableFrom(t) && filter(t)).
-                ToList();
+
+            if (types == null)
+            {
+                types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes());
+            }
+
+            if (subclasses == null)
+            {
+                subclasses = new Dictionary<Type, IEnumerable<Type>>();
+            }
+
+            if (!subclasses.ContainsKey(type))
+            {
+                subclasses[type] = types.Where(t => t.IsClass && type.IsAssignableFrom(t));
+            }
+
+            return subclasses[type].Where(filter).ToList();
         }
 
         public static bool Implements(this Type type, params Type[] interfaces)
