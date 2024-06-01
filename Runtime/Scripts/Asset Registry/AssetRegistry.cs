ab1879f9ab33d2375178f2ff8df8608d9fe5b9bf
snarlynarwhal <pjlegendreiv@gmail.com>
2024-06-01 19:16:06 -0400

    Updated AssetRegistry to auto-load if a guid/object lookup fails (so don't need to manually update anymore)

--- Runtime/Scripts/Asset Registry/AssetRegistry.cs
+++ Runtime/Scripts/Asset Registry/AssetRegistry.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Linq;
 using System.Collections.Generic;
 using System;
+
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -20,14 +21,54 @@ namespace HHG.Common.Runtime
 
         [SerializeField] private AssetFolder[] folders;
         [SerializeField] private SerializableDictionary<string, Object> assets = new SerializableDictionary<string, Object>();
-        
+
+        // This doesn't need to be serialzied since it gets initialized after deserialize
         private Dictionary<Object, string> guids = new Dictionary<Object, string>();
 
         public static T GetAsset<T>(string guid) where T : Object => Instance.getAsset<T>(guid);
         public static string GetGuid(Object asset) => Instance.getGuid(asset);
 
-        public T getAsset<T>(string guid) where T : Object => !string.IsNullOrEmpty(guid) && assets.ContainsKey(guid) ? assets[guid] as T : null;
-        public string getGuid(Object asset) => asset != null && guids.ContainsKey(asset) ? guids[asset] : null;
+        public T getAsset<T>(string guid) where T : Object
+        {
+            if (string.IsNullOrEmpty(guid))
+            {
+                return null;
+            }
+
+            if (assets.TryGetValue(guid, out var asset))
+            {
+                return asset as T;
+            }
+
+#if UNITY_EDITOR
+            editorLoad();
+            assets.TryGetValue(guid, out asset);
+#endif
+
+            return asset as T;
+        }
+
+
+        public string getGuid(Object asset)
+        {
+            if (asset == null)
+            {
+                return null;
+            }
+
+            if (guids.TryGetValue(asset, out var guid))
+            {
+                return guid;
+            }
+
+#if UNITY_EDITOR
+            editorLoad();
+            guids.TryGetValue(asset, out guid);
+#endif
+
+            return guid;
+        }
+
 
         public void OnBeforeSerialize()
         {
@@ -47,25 +88,29 @@ namespace HHG.Common.Runtime
         private void editorLoad()
         {
             assets.Clear();
+            guids.Clear();
             foreach (AssetFolder folder in folders)
             {
                 string filter = string.Join(' ', folder.Filter.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(f => $"t:{f}"));
-                string[] guids = AssetDatabase.FindAssets($"{filter}", new[] { folder.Folder });
-                var paths = guids.Select(guid => AssetDatabase.GUIDToAssetPath(guid));
+                string[] ids = AssetDatabase.FindAssets($"{filter}", new[] { folder.Folder });
+                var paths = ids.Select(id => AssetDatabase.GUIDToAssetPath(id));
                 var objs = paths.Select(path => AssetDatabase.LoadAssetAtPath<Object>(path));
                 foreach (Object obj in objs)
                 {
                     string path = AssetDatabase.GetAssetPath(obj);
-                    string guid = AssetDatabase.AssetPathToGUID(path);
-                    if (assets.ContainsKey(guid))
+                    string id = AssetDatabase.AssetPathToGUID(path);
+                    if (assets.ContainsKey(id))
                     {
                         Debug.LogError("Asset registry already contains asset: " + path, obj);
                         continue;
                     }
-                    assets.Add(guid, obj);
+                    assets.Add(id, obj);
+                    guids.Add(obj, id);
                 }
             }
             EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
 
         [MenuItem("Half Human Games/Asset Registry")]
