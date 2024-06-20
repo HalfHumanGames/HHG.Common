e4dbaf21c5573e5453f455f9075667d7c0af8b0e
snarlynarwhal <pjlegendreiv@gmail.com>
2024-06-20 16:47:27 -0400

    Added AudioSource extension methods: FadeTo, FadeToAsync, FadeToDelayed, FadeToDelayedAsync

---
+++ Runtime/Scripts/Extensions/AudioSourceExtensions.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace HHG.Common.Runtime
+{
+    public static class AudioSourceExtensions
+    {
+        public static Coroutine FadeTo(this AudioSource source, float target, float duration = 1f, Func<float, float> ease = null)
+        {
+            return CoroutineUtil.StartCoroutine(source.FadeToAsync(target, duration, ease));
+        }
+
+        public static IEnumerator FadeToAsync(this AudioSource source, float target, float duration = 1f, Func<float, float> ease = null)
+        {
+            yield return FadeToInternalAsync(source, 0f, target, duration, ease);
+        }
+
+        public static Coroutine FadeToDelayed(this AudioSource source, float delay, float target, float duration = 1f, Func<float, float> ease = null)
+        {
+            return CoroutineUtil.StartCoroutine(source.FadeToDelayedAsync(delay, target, duration, ease));
+        }
+
+        public static IEnumerator FadeToDelayedAsync(this AudioSource source, float delay, float target, float duration = 1f, Func<float, float> ease = null)
+        {
+            yield return FadeToInternalAsync(source, delay, target, duration, ease);
+        }
+
+        private static IEnumerator FadeToInternalAsync(AudioSource source, float delay, float target, float duration, Func<float, float> ease)
+        {
+            if (target > 0f && !source.isPlaying)
+            {
+                if (delay > 0f)
+                {
+                    source.PlayDelayed(delay);
+                    yield return new WaitForSeconds(delay);
+                }
+                else
+                {
+                    source.Play();
+                }
+            }
+
+            yield return TweenUtil.TweenAsync(() => source.volume, v => source.volume = v, target, duration, ease);
+
+            if (target <= 0f)
+            {
+                source.Stop();
+            }
+        }
+    }
+}
