1f3ac54f7d566ed06bd9119fba89153df98f3514
snarlynarwhal <pjlegendreiv@gmail.com>
2024-07-11 16:42:53 -0400

    Updated RendererGroup to update in OnTransformChildrenChanged

--- Runtime/Scripts/Helper Components/RendererGroup.cs
+++ Runtime/Scripts/Helper Components/RendererGroup.cs
@@ -4,6 +4,8 @@ namespace HHG.Common.Runtime
 {
     public class RendererGroup : MonoBehaviour
     {
+        private bool isEnabled;
+
         public void Enable()
         {
             Enable(true);
@@ -16,12 +18,19 @@ namespace HHG.Common.Runtime
 
         public void Enable(bool enable)
         {
+            isEnabled = enable;
+
             Renderer[] renderers = GetComponentsInChildren<Renderer>();
 
             foreach (Renderer renderer in renderers)
             {
                 renderer.enabled = enable;
             }
+        }
+
+        private void OnTransformChildrenChanged()
+        {
+            Enable(isEnabled);
         }
     } 
 }
