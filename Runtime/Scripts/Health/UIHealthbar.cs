be956d63995470e66d9be98540081c79ab95a9c8
snarlynarwhal <pjlegendreiv@gmail.com>
2024-08-19 12:05:47 -0400

    Optimized in-game healthbars

--- Runtime/Scripts/Health/UIHealthbar.cs
+++ Runtime/Scripts/Health/UIHealthbar.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 namespace HHG.Common.Runtime
 {
+    [RequireComponent(typeof(CanvasRenderer))]
+    [RequireComponent(typeof(CanvasGroup))]
     public class UIHealthbar : MonoBehaviour
     {
         [SerializeField] private Transform fill;
         [SerializeField] private Transform background;
 
-        protected IReadOnlyHealth source;
-
+        private IReadOnlyHealth source;
+        private CanvasGroup canvasGroup;
         private float scale;
 
         private void Start()
         {
-            source = GetComponentInParent<IReadOnlyHealth>();
+            source = GetComponentInParent<IReadOnlyHealth>(true);
+            canvasGroup = GetComponent<CanvasGroup>();
             scale = fill.transform.localScale.x;
         }
 
@@ -22,7 +25,7 @@ namespace HHG.Common.Runtime
             float perc = source.HealthPerc;
             float scaleX = perc * scale;
             bool isVisible = perc < 1f;
-            gameObject.SetActive(isVisible);
+            canvasGroup.alpha = isVisible ? 1f : 0f;
             fill.transform.localScale = fill.transform.localScale.WithX(scaleX);
         }
     }
