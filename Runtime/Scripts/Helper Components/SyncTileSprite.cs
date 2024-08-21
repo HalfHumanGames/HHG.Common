4e7438aa7a9ab0b7d074eac88fe74ec807b4dfec
snarlynarwhal <pjlegendreiv@gmail.com>
2024-08-21 19:38:36 -0400

    Extended SyncTileSprite to include option to use a SpriteLookupMapAsset when syncing tile sprites.

--- Runtime/Scripts/Helper Components/SyncTileSprite.cs
+++ Runtime/Scripts/Helper Components/SyncTileSprite.cs
@@ -5,32 +5,63 @@ namespace HHG.Common.Runtime
 {
     public class SyncTileSprite : MonoBehaviour
     {
+        [SerializeField] private bool useLookupMap;
+        [SerializeField] private SpriteLookupMapAsset lookupMap;
+
         private SpriteRenderer spriteRenderer;
         private Tilemap tilemap;
         private Vector3Int position;
+        private bool hasStarted;
 
         private void Start()
         {
             // Initialize in Start since not added to tilemap in Awake
-            spriteRenderer = GetComponent<SpriteRenderer>();
+            // Transform gets parented to tilemap between Awake and Start
             tilemap = GetComponentInParent<Tilemap>(true);
             position = tilemap.WorldToCell(transform.position);
+            spriteRenderer = GetComponent<SpriteRenderer>();
             spriteRenderer.sprite = tilemap.GetSprite(position);
+            
+            SyncSprite(); // Since not called in first OnEnable
+
+            hasStarted = true;
+        }
+
+        private void OnEnable()
+        {
+            if (hasStarted)
+            {
+                SyncSprite();
+            }
 
             Tilemap.tilemapTileChanged += OnTileChanged;
+        }
+
+        private void OnDisable()
+        {
+            Tilemap.tilemapTileChanged -= OnTileChanged;
         }
 
         private void OnTileChanged(Tilemap map, Tilemap.SyncTile[] syncTiles)
         {
             if (map == tilemap)
             {
-                spriteRenderer.sprite = tilemap.GetSprite(position);
+                SyncSprite();
             }
         }
 
-        private void OnDestroy()
+        private void SyncSprite()
         {
-            Tilemap.tilemapTileChanged -= OnTileChanged;
+            Sprite sprite = tilemap.GetSprite(position);
+
+            if (useLookupMap && lookupMap && lookupMap.Map.TryGetValue(sprite, out sprite))
+            {
+                spriteRenderer.sprite = sprite;
+            }
+            else
+            {
+                spriteRenderer.sprite = sprite;
+            }
         }
     }
 }
