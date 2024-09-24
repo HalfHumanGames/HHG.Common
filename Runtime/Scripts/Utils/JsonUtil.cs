6fa42626a1e18390d5702c71b989c4fb3b905091
snarlynarwhal <pjlegendreiv@gmail.com>
2024-09-24 19:14:13 -0400

    Added JsonUtil.ToJson/FromJson which uses Unity's JsonUtility, but serialize to/from binary data, aka byte[]

--- Runtime/Scripts/Utils/JsonUtil.cs
+++ Runtime/Scripts/Utils/JsonUtil.cs
@@ -5,6 +5,16 @@ namespace HHG.Common.Runtime
 {
     public static class JsonUtil
     {
+        public static byte[] ToJson<T>(this T value, bool pretty = false)
+        {
+            return Encoding.UTF8.GetBytes(JsonUtility.ToJson(value, pretty));
+        }
+
+        public static T FromJson<T>(byte[] bytes)
+        {
+            return JsonUtility.FromJson<T>(Encoding.UTF8.GetString(bytes));
+        }
+
         public static byte[] ToGZipJson<T>(this T value)
         {
             return GZipUtil.Compress(Encoding.UTF8.GetBytes(JsonUtility.ToJson(value)));
