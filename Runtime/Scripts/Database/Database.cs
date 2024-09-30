fcc25e8e4874d2ff8353a3f0f12a89b2f409157b
snarlynarwhal <pjlegendreiv@gmail.com>
2024-09-30 13:14:14 -0400

    Refactored Database.cs

--- Runtime/Scripts/Database/Database.cs
+++ Runtime/Scripts/Database/Database.cs
@@ -10,48 +10,105 @@ namespace HHG.Common.Runtime
     {
         private static Dictionary<Type, Dictionary<string, Object>> dict = new Dictionary<Type, Dictionary<string, Object>>();
 
+        public static bool TryGet<T>(string name, out T val) where T : Object
+        {
+            val = default;
+
+            if (name == null)
+            {
+                return false;
+            }
+
+            EnsureLoaded<T>();
+
+            Type key = typeof(T);
+
+            return dict[key].TryGetValue(name, out Object obj) && (val = obj as T);
+        }
+
         public static T Get<T>(string name) where T : Object
         {
-            EnsureLoaded<T>();
-            Type key = typeof(T);
-            return dict[key].ContainsKey(name) ? dict[key][name] as T : default;
+            return TryGet(name, out T val) ? val : default;
         }
 
-        public static T Get<T>(Func<T, bool> search = null) where T : Object
+        public static T Get<T>() where T : Object
         {
-            search ??= _ => true;
             EnsureLoaded<T>();
-            return GetAll<T>().FirstOrDefault(search);
+
+            Type key = typeof(T);
+
+            return dict[key].Values.OfType<T>().FirstOrDefault();
         }
 
-        public static T[] GetAll<T>(Func<T, bool> filter = null) where T : Object
+        public static T Get<T>(Func<T, bool> search) where T : Object
+        {
+            if (search == null)
+            {
+                return Get<T>();
+            }
+
+            EnsureLoaded<T>();
+
+            Type key = typeof(T);
+
+            return dict[key].Values.OfType<T>().FirstOrDefault(search);
+        }
+
+        public static T[] GetAll<T>() where T : Object
         {
             EnsureLoaded<T>();
+
             Type key = typeof(T);
-            filter ??= _ => true;
-            return dict[key].Values.Select(item => item as T).Where(filter).ToArray();
+
+            return dict[key].Values.OfType<T>().ToArray();
+        }
+
+        public static T[] GetAll<T>(Func<T, bool> filter) where T : Object
+        {
+            if (filter == null)
+            {
+                return GetAll<T>();
+            }
+
+            EnsureLoaded<T>();
+
+            Type key = typeof(T);
+
+            return dict[key].Values.OfType<T>().Where(filter).ToArray();
         }
 
         public static bool Contains<T>(string name) where T : Object
         {
+            if (name == null)
+            {
+                return false;
+            }
+
             EnsureLoaded<T>();
+
             Type key = typeof(T);
+
             return dict[key].ContainsKey(name);
         }
 
         private static void EnsureLoaded<T>() where T : Object
         {
             Type key = typeof(T);
-            if (dict.ContainsKey(key)) return;
-            dict[key] = new Dictionary<string, Object>();
-            T[] items = Resources.LoadAll<T>("");
-            foreach (T item in items)
+
+            if (!dict.ContainsKey(key))
             {
-                if (item is not IEnablable enablable || enablable.IsEnabled)
+                dict[key] = new Dictionary<string, Object>();
+
+                T[] items = Resources.LoadAll<T>(string.Empty);
+
+                foreach (T item in items)
                 {
-                    dict[key][item.name] = item;
+                    if (item is not IEnablable enablable || enablable.IsEnabled)
+                    {
+                        dict[key][item.name] = item;
+                    }
                 }
             }
         }
-    } 
+    }
 }
