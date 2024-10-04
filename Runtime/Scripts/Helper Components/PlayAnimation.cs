bc8ecbcbf2f6d185d4fb136ac605f3e3b557decf
snarlynarwhal <pjlegendreiv@gmail.com>
2024-10-04 01:31:29 -0400

    Added PlayAnimation helper script (for simple anims where don't need AnimatorController)

---
+++ Runtime/Scripts/Helper Components/PlayAnimation.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+namespace HHG.Common.Runtime
+{
+    [RequireComponent(typeof(Animator))]
+    public class PlayAnimation : MonoBehaviour
+    {
+        [SerializeField] private AnimationClip animationClip;
+        
+        private PlayableGraph playableGraph;
+
+        private void Start()
+        {
+            playableGraph = PlayableGraph.Create();
+            playableGraph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
+
+            AnimationPlayableOutput playableOutput = AnimationPlayableOutput.Create(playableGraph, "Animation", GetComponent<Animator>());
+
+            // Wrap the clip in a playable.
+            AnimationClipPlayable clipPlayable = AnimationClipPlayable.Create(playableGraph, animationClip);
+
+            // Connect the Playable to an output.
+            playableOutput.SetSourcePlayable(clipPlayable);
+
+            // Plays the Graph.
+            playableGraph.Play();
+        }
+
+        private void OnDestroy()
+        {
+            // Destroys all Playables and PlayableOutputs created by the graph.
+            playableGraph.Destroy();
+        }
+    } 
+}
