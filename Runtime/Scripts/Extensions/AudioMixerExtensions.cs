ee5010440f2023b35ceeafea9cfbca94252f3cc9
snarlynarwhal <pjlegendreiv@gmail.com>
2024-10-04 16:37:35 -0400

    Fixed normalized volume not being set properly

--- Runtime/Scripts/Extensions/AudioMixerExtensions.cs
+++ Runtime/Scripts/Extensions/AudioMixerExtensions.cs
@@ -5,12 +5,10 @@ namespace HHG.Common.Runtime
 {
     public static class AudioMixerExtensions
     {
-        private const float maxDb = 20f;
-        private const float minDb = -80f;
-
         public static void SetVolumeNormalized(this AudioMixer mixer, string group, float normalized)
         {
-            float db = Mathf.Clamp(minDb + (normalized * (maxDb - minDb)), minDb, maxDb);
+            normalized = Mathf.Clamp(normalized, .0001f, 1f);
+            float db = Mathf.Log10(normalized) * 20f;
             mixer.SetFloat(group, db);
         }
     }
