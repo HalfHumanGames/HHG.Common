20264be896625378c23c729812eea84176926412
snarlynarwhal <pjlegendreiv@gmail.com>
2025-01-21 15:07:38 -0500

    Added DontDestroyOnLoad helper script.

---
+++ Runtime/Scripts/Helper Components/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace HHG.Common.Runtime
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
