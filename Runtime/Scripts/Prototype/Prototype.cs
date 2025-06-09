7a643dde894b5d9e690cf7f38767ce2ef0e0226b
snarlynarwhal <pjlegendreiv@gmail.com>
2025-06-09 13:33:47 -0400

    Uncommented out Prototype script and also updated it so it works with prefabs (not just original source instances that already exist in the scene).

--- Runtime/Scripts/Prototype/Prototype.cs
+++ Runtime/Scripts/Prototype/Prototype.cs
@@ -1,119 +1,116 @@
-//using UnityEngine;
-//using System.Collections.Generic;
-//using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
 
-//namespace HHG.Common.Runtime
-//{
-//    /// <summary>
-//    /// Convenience way to create objects in the hierarchy that are a bit like prefabs, except that they
-//    /// aren't. They exist in the scene, immediately deactivate themselves, then provide an easy way to
-//    /// instantiate a copy in the same place in the hierarchy.
-//    /// If you call ReturnToPool() once you're done with the instance, then it's returned
-//    /// to the original prototype to be reused if necessary instead of creating a brand new GameObject.
-//    /// </summary>
-//    public class Prototype : MonoBehaviour
-//    {
-//        public bool IsOriginal => original == null;
-//        public Prototype Original => original;
-//        public event Action OnReturnToPool;
+namespace HHG.Common.Runtime
+{
+    public class Prototype : MonoBehaviour
+    {
+        public bool IsOriginal => original == null;
+        public Prototype Original => original;
+        public event Action OnReturnToPool;
 
-//        private Prototype original;
-//        private List<Prototype> pool;
+        private Prototype original;
+        private List<Prototype> pool;
 
-//        void Start()
-//        {
-//            if (IsOriginal)
-//            {
-//                gameObject.SetActive(false);
-//            }
-//        }
+        private void Start()
+        {
+            if (IsOriginal)
+            {
+                gameObject.SetActive(false);
+            }
+        }
 
-//        void OnDestroy()
-//        {
-//            if (pool != null)
-//            {
-//                foreach (Prototype proto in pool)
-//                {
-//                    Destroy(proto);
-//                }
-//            }
-//        }
+        private void OnDestroy()
+        {
+            if (pool != null)
+            {
+                foreach (Prototype proto in pool)
+                {
+                    Destroy(proto);
+                }
 
-//        public T Instantiate<T>() where T : Component
-//        {
-//            Prototype proto;
+                pool.Clear();
+            }
+            
+            if (original != null)
+            {
+                // It does not matter if removal
+                // succeeds or not, it is faster
+                // to skip the contains check.
+                original.pool.Remove(this);
+            }
+        }
 
-//            // Re-use instance from pool
-//            if (pool != null && pool.Count > 0)
-//            {
-//                var instanceIdx = pool.Count - 1;
-//                proto = pool[instanceIdx];
-//                pool.RemoveAt(instanceIdx);
-//            }
+        public T Instantiate<T>() where T : Component
+        {
+            Prototype proto;
 
-//            // Instantiate fresh instance
-//            else
-//            {
-//                proto = Instantiate(this);
+            if (pool != null && pool.Count > 0)
+            {
+                var instanceIdx = pool.Count - 1;
+                proto = pool[instanceIdx];
+                pool.RemoveAt(instanceIdx);
+            }
+            else
+            {
+                proto = Instantiate(this);
+                proto.transform.SetParent(transform.parent, worldPositionStays: false);
 
-//                proto.transform.SetParent(transform.parent, worldPositionStays: false);
+                var rectTransform = transform as RectTransform;
+                if (rectTransform)
+                {
+                    var protoRectTransform = proto.transform as RectTransform;
+                    protoRectTransform.anchorMin = rectTransform.anchorMin;
+                    protoRectTransform.anchorMax = rectTransform.anchorMax;
+                    protoRectTransform.pivot = rectTransform.pivot;
+                    protoRectTransform.sizeDelta = rectTransform.sizeDelta;
+                }
 
-//                var rectTransform = transform as RectTransform;
-//                if (rectTransform)
-//                {
-//                    var protoRectTransform = proto.transform as RectTransform;
-//                    protoRectTransform.anchorMin = rectTransform.anchorMin;
-//                    protoRectTransform.anchorMax = rectTransform.anchorMax;
-//                    protoRectTransform.pivot = rectTransform.pivot;
-//                    protoRectTransform.sizeDelta = rectTransform.sizeDelta;
-//                }
+                proto.transform.localPosition = transform.localPosition;
+                proto.transform.localRotation = transform.localRotation;
+                proto.transform.localScale = transform.localScale;
+                proto.original = this;
+            }
 
-//                proto.transform.localPosition = transform.localPosition;
-//                proto.transform.localRotation = transform.localRotation;
-//                proto.transform.localScale = transform.localScale;
+            proto.gameObject.SetActive(true);
+            return proto.GetComponent<T>();
+        }
 
-//                proto.original = this;
-//            }
+        public void ReturnToPool()
+        {
+            if (IsOriginal)
+            {
+                Debug.LogError("Can't return to pool because the original prototype doesn't exist. Is this prototype the original?");
+                Destroy(gameObject);
+                return;
+            }
 
-//            proto.gameObject.SetActive(true);
+            original.AddToPool(this);
+        }
 
-//            return proto.GetComponent<T>();
-//        }
+        public T GetOriginal<T>()
+        {
+            return IsOriginal ? GetComponent<T>() : Original.GetComponent<T>();
+        }
 
-//        public void ReturnToPool()
-//        {
-//            if (IsOriginal)
-//            {
-//                Debug.LogError("Can't return to pool because the original prototype doesn't exist. Is this prototype the original?");
-//                Destroy(gameObject);
-//                return;
-//            }
+        private void AddToPool(Prototype proto)
+        {
+            if (!IsOriginal)
+            {
+                Debug.LogError("Adding " + proto.name + " to prototype pool of " + name + " but this appears to be an instance itself?");
+            }
 
-//            original.AddToPool(this);
-//        }
+            proto.gameObject.SetActive(false);
 
-//        public T GetOriginal<T>()
-//        {
-//            return IsOriginal ? GetComponent<T>() : Original.GetComponent<T>();
-//        }
+            if (pool == null)
+            {
+                pool = new List<Prototype>();
+            }
 
-//        void AddToPool(Prototype proto)
-//        {
-//            if (!IsOriginal)
-//            {
-//                Debug.LogError("Adding " + proto.name + " to prototype pool of " + name + " but this appears to be an instance itself?");
-//            }
+            pool.Add(proto);
 
-//            proto.gameObject.SetActive(false);
-
-//            if (pool == null)
-//            {
-//                pool = new List<Prototype>();
-//            }
-
-//            pool.Add(proto);
-
-//            proto.OnReturnToPool?.Invoke();
-//        }
-//    } 
-//}
+            proto.OnReturnToPool?.Invoke();
+        }
+    }
+}
