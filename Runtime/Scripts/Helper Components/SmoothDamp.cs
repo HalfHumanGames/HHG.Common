bbac8c06b8af38b7be2022756b431aa6c55bc435
snarlynarwhal <pjlegendreiv@gmail.com>
2025-07-28 19:35:15 -0400

    Added max speed support to SmoothDamp script.

--- Runtime/Scripts/Helper Components/SmoothDamp.cs
+++ Runtime/Scripts/Helper Components/SmoothDamp.cs
@@ -6,13 +6,14 @@ namespace HHG.Common.Runtime
     {
         [SerializeField] private Transform target;
         [SerializeField] private float smoothTime = 0.3f;
+        [SerializeField] private float maxSpeed = 1000f;
         [SerializeField] private Vector3 offset;
 
         private Vector3 velocity = Vector3.zero;
 
-        void Update()
+        private void Update()
         {
-            transform.position = Vector3.SmoothDamp(transform.position, target.TransformPoint(offset), ref velocity, smoothTime);
+            transform.position = Vector3.SmoothDamp(transform.position, target.TransformPoint(offset), ref velocity, smoothTime, maxSpeed);
         }
     } 
 }
