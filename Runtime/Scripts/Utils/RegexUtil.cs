4f3d6d4ef7750e450b58713c3d77f868b8de2ecf
snarlynarwhal <pjlegendreiv@gmail.com>
2025-07-28 15:51:13 -0400

    Misc renaming/refactor.

--- Runtime/Scripts/Utils/RegexUtil.cs
+++ Runtime/Scripts/Utils/RegexUtil.cs
@@ -4,18 +4,18 @@ namespace HHG.Common.Runtime
 {
     public static class RegexUtil
     {
-        public static float ExtractFloat(string input)
+        public static float ToFloat(string input)
         {
             float value = float.Parse(Regex.Match(input, @"-?\d*\.?\d+").Value);
             return input.Contains('%') ? value / 100f : value;
         }
 
-        public static int ExtractInt(string input)
+        public static int ToInt(string input)
         {
             return int.Parse(Regex.Match(input, @"-?\d+").Value);
         }
 
-        public static bool ExtractBool(string input)
+        public static bool ToBool(string input)
         {
             return bool.Parse(Regex.Match(input, @"\b(true|false)\b", RegexOptions.IgnoreCase).Value.ToLower());
         }
