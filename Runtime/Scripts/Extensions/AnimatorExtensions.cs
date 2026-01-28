2628490b1e4a647e060cfb7a90973bf62974523b
snarlynarwhal <pjlegendreiv@gmail.com>
2026-01-28 14:57:34 -0500

    Added AnimatorExtensions.Offset so instances of same animator don't play in sync.

---
+++ Runtime/Scripts/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace HHG.Common.Runtime
+{
+    public static class AnimatorExtensions
+    {
+        public static void Offset(this Animator animator, int layer = 0)
+        {
+            AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(layer);
+            float offset = Random.Range(0f, 1f);
+            animator.Play(info.fullPathHash, layer, offset);
+        }
+    }
+}
