01c41999ad523a59f059d7e43737793b915fa323
snarlynarwhal <pjlegendreiv@gmail.com>
2026-03-25 11:49:15 -0400

    Added EditorUtil.CanValidate utility method.

--- Editor/Scripts/Utils/EditorUtil.cs
+++ Editor/Scripts/Utils/EditorUtil.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection;
 using UnityEditor;
+using UnityEngine;
 
 namespace HHG.Common.Editor
 {
@@ -11,6 +12,11 @@ namespace HHG.Common.Editor
             Type type = typeof(ProjectWindowUtil);
             MethodInfo getter = type.GetMethod("GetActiveFolderPath", BindingFlags.Static | BindingFlags.NonPublic);
             return getter.Invoke(null, new object[0]).ToString();
+        }
+
+        public static bool CanValidate()
+        {
+            return !Application.isPlaying && !EditorApplication.isCompiling && !EditorApplication.isUpdating && !BuildPipeline.isBuildingPlayer;
         }
     }
 }
