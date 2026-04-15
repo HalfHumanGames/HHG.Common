522279f14146b77ea5de94bb1ccd34bad43cf450
snarlynarwhal <pjlegendreiv@gmail.com>
2026-04-15 19:22:00 -0400

    Updated Typewriter.Typewrite to handle tags and made action param optional.

--- Runtime/Scripts/Typewriter/Typewriter.cs
+++ Runtime/Scripts/Typewriter/Typewriter.cs
@@ -12,20 +12,34 @@ namespace HHG.Common.Runtime
         private static float fullPause = .5f;
         private static float quarterPause = .05f;
 
-        public static IEnumerator Typewrite(TextMeshProUGUI text, string line, InputAction action)
+        public static IEnumerator Typewrite(TMP_Text text, string line, InputAction action = null)
         {
             text.text = string.Empty;
 
             for (int i = 0; i < line.Length; i++)
             {
+                char currentChar = line[i];
+
+                // If we hit an opening tag, skip to the end of the tag
+                if (currentChar == '<')
+                {
+                    int tagEndIndex = line.IndexOf('>', i);
+                    if (tagEndIndex != -1)
+                    {
+                        // Include the entire tag in the output immediately
+                        i = tagEndIndex;
+                        text.text = line.Substring(0, i + 1);
+                        continue; // Continue to next character for tags
+                    }
+                }
+
                 text.text = line.Substring(0, i + 1);
 
-                float delay = Array.IndexOf(punctuation, text.text[i]) < 0 ? quarterPause : fullPause;
+                float delay = Array.IndexOf(punctuation, currentChar) < 0 ? quarterPause : fullPause;
 
-                if (action.IsPressed())
+                if (action != null && action.IsPressed())
                 {
                     text.text = line;
-
                     yield break;
                 }
                 else
