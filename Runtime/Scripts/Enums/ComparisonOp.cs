78cc6ad6b1ae375ee1e9d447fc568e5bfa57d27e
snarlynarwhal <pjlegendreiv@gmail.com>
2026-05-15 23:16:49 -0400

    Added ComparisonOp enum and Evaluate extension method.

---
+++ Runtime/Scripts/Enums/ComparisonOp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace HHG.Common.Runtime
+{
+    public enum ComparisonOp
+    {
+        GreaterThan,
+        GreaterThanEqualTo,
+        LessThan,
+        LessThanEqualTo,
+        EqualTo,
+        NotEqualTo
+    }
+
+    public static class ComparisonOpExtensions
+    {
+        public static bool Evaluate(this ComparisonOp op, float value, float threshold) => op switch
+        {
+            ComparisonOp.GreaterThan => value > threshold,
+            ComparisonOp.GreaterThanEqualTo => value >= threshold,
+            ComparisonOp.LessThan => value < threshold,
+            ComparisonOp.LessThanEqualTo => value <= threshold,
+            ComparisonOp.EqualTo => Mathf.Approximately(value, threshold),
+            ComparisonOp.NotEqualTo => !Mathf.Approximately(value, threshold),
+            _ => false
+        };
+    }
+}
