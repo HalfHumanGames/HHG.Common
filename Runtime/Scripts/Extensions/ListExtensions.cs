482a1e724bb6d5833cde0372bb21edff1ad3fbee
snarlynarwhal <pjlegendreiv@gmail.com>
2026-06-24 14:26:57 -0400

    Updated ListExtensions.IndexOf to include optional IEqualityComparer parameter.

--- Runtime/Scripts/Extensions/ListExtensions.cs
+++ Runtime/Scripts/Extensions/ListExtensions.cs
@@ -111,17 +111,12 @@ namespace HHG.Common.Runtime
             list.SortedInsert(item, comparer);
         }
 
-        public static int IndexOf<T>(this IReadOnlyList<T> list, T item)
+        public static int IndexOf<T>(this IReadOnlyList<T> list, T item, IEqualityComparer<T> comparer = null)
         {
-            int i = 0;
-            foreach (T element in list)
+            comparer ??= EqualityComparer<T>.Default;
+            for (int i = 0; i < list.Count; i++)
             {
-                if (Equals(element, item))
-                {
-                    return i;
-                }
-                    
-                i++;
+                if (comparer.Equals(list[i], item)) return i;
             }
             return -1;
         }
