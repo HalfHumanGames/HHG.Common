102bc21d76549afc56daee70064885f937a30fe2
snarlynarwhal <pjlegendreiv@gmail.com>
2026-06-30 20:46:23 -0400

    Updated SceneObjectCollectionAsset to implement IEnumerable<SceneObjectReference>.

--- Runtime/Scripts/Scene Registry/SceneObjectCollectionAsset.cs
+++ Runtime/Scripts/Scene Registry/SceneObjectCollectionAsset.cs
@@ -1,23 +1,34 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace HHG.Common.Runtime
 {
     [CreateAssetMenu(fileName = "Scene Object Collection", menuName = "HHG/Assets/Scene Object Collection")]
-    public class SceneObjectCollectionAsset : ScriptableObject
+    public class SceneObjectCollectionAsset : ScriptableObject, IEnumerable<SceneObjectReference>
     {
-        public IReadOnlyList<SceneObjectReference> References => references;
+        public IReadOnlyList<SceneObjectReference> Items => items;
 
         [SerializeField] private string registryName;
 
         [EditorButton(nameof(AddReference), "+")]
-        [SerializeField, SceneObjectRegistry] private List<SceneObjectReference> references = new List<SceneObjectReference>();
+        [SerializeField, SceneObjectRegistry] private List<SceneObjectReference> items = new List<SceneObjectReference>();
 
 #if UNITY_EDITOR
         private void AddReference()
         {
-            references.Add(default);
+            items.Add(default);
             UnityEditor.EditorUtility.SetDirty(this);
+        }
+
+        public IEnumerator<SceneObjectReference> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
 #endif
     }
